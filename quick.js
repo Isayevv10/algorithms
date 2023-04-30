e4584a8d678061322a0635543ac13997547983ea
Isayev Allahverdi <isaevallahverdi@gmail.com>
2023-04-30 21:52:38 +0400

    done

--- quick.js
+++ quick.js
@@ -2,21 +2,17 @@ function quickSort(array) {
   if (array.length < 2) {
     return array;
   }
-  let pivot_element = array[array.length - 1];
-  let left_sub_array = [];
-  let right_sub_array = [];
+  let pivot = array[array.length - 1];
+  let left_arr = [];
+  let right_arr = [];
   for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] < pivot_element) {
-      left_sub_array.push(array[i]);
+    if (array[i] < pivot) {
+      left_arr.push(array[i]);
     } else {
-      right_sub_array.push(array[i]);
+      right_arr.push(array[i]);
     }
   }
-  return [
-    ...quickSort(left_sub_array),
-    pivot_element,
-    ...quickSort(right_sub_array),
-  ];
+  return [...quickSort(left_arr), pivot, ...quickSort(right_arr)];
 }
 const array = [5, 4, 8, 7, 12, 1];
 console.log(quickSort(array));
